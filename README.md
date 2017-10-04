e74f8a0e0c437f0a78cf0e64f925d787f504912b
Sam Keller <sam@navahq.com>
2017-10-04 14:24:30 -0600

    Basic functionality for file upload workflow
    Closes #1
    
    The module, as of now, exports a single function, fileUploader() which
    accepts the following as parameters:
    
    submissionBody -- a string for the body of the submission from the file
    being uploaded
    submissionFormat -- a string which should specify the format of the
    submission body; either 'JSON' or 'XML'
    JWT -- the JSON Web Token (a string) to be used when making requests to
    the Submissions API
    baseSubmissionURL -- a string for the URL to be used (helpful to have it
    not default to prod for testing)
    callback -- a callback function to be called once the workflow has
    finished, which takes an array of Error objects that were thrown as the
    first argument and an array of created measurementSet as the second.
    The module then follows the business logic outlined in a Confluence
    page to validate the submissionBody, query for any existing submissions,
    and then using POST /measurement-sets and PUT /measurement-sets
    appropriately to save the measurementSets in the submissionBody in the
    Submissions API database.
    
    A set of unit tests have been written to test basic functionality as
    well.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # qpp-file-upload-api-client
-A set of functions to call the QPP Submissions API in common manner, such as for the file upload use case. 
+A set of functions to call the QPP Submissions API in common manner, such as for the file upload use case.
+
+To use, simply `npm install qpp-file-upload-api-client`
+
+## fileUploader()
+The `fileUploader()` function exported by this module uses the following set of requests on the Submissions API:
+
+1. Parse the submission object and call POST /submissions/validate to ensure that it's a valid submission
+1. Call GET /submissions with query parameters to find any matching submission and measurementSets
+1. Given the result of GET /submissions, determine whether to PUT or POST measurementSets from the file being uploaded
+
+Arguments
+---------
+
+1. `submissionBody`: A string containing the QPP JSON or QPP XML Submission body. See the [Submissions API Developer Documentation](cmsgov.github.io/qpp-submissions-docs) for more information on schemas, but you can use this example payload to use with the `fileUploader()` function: [example payload](https://gist.github.com/samskeller/0eeb89ead1ddb189236593e2a9aa1034)
+1. `submissionFormat`: A string specifying the format -- only "JSON" and "XML" are supported
+1. `JWT`: A string with the user's JSON Web Token (JWT)
+1. `baseSubmissionURL`: A string with the base URL to be used to specify the Submissions API -- for the production instance of the Submissions API, for example, you'd use `https://qpp.cms.gov/api/submissions`
+1. `callback`: A callback function to be called when the `fileUploader()` function is finished. Should accept an array of Error objects as the first argument, for the list of Errors that were thrown during the function call, and an array of created measurementSets (Objects) as the second argument
+
+## Testing
+To run the automated tests for this module, simply run:
+
+```bash
+npm test
+```
---
+++ test/file-uploader-util-spec.js
@@ -0,0 +1,303 @@
+const assert = require('chai').assert;
+const sinon = require('sinon');
+const rp = require('request-promise');
+
+const {
+  validateSubmission,
+  getExistingSubmission,
+  postMeasurementSet,
+  putMeasurementSet,
+  submitMeasurementSets
+} = require('../file-uploader-util');
+
+const baseOptions = {
+  url: '',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+};
+
+const validSubmission = {
+  programName: 'mips',
+  entityType: 'individual',
+  taxpayerIdentificationNumber: '000123456',
+  nationalProviderIdentifier: '0123456789',
+  performanceYear: 2017,
+  measurementSets: [{
+    category: 'ia',
+    submissionMethod: 'registry',
+    performanceStart: '2017-01-01',
+    performanceEnd: '2017-06-01',
+    measurements: [{
+      measureId: 'IA_EPA_4',
+      value: true
+    }]
+  }]
+};
+
+describe('fileUploaderUtils', () => {
+  const sandbox = sinon.sandbox.create();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('validateSubmission', () => {
+    let rpPostStub;
+    beforeEach(() => {
+      rpPostStub = sandbox.stub(rp, 'post').returns(new Promise((resolve, reject) => {
+        resolve(JSON.stringify({
+          data: {
+            submission: validSubmission
+          }
+        }));
+      }));
+    });
+
+    it('makes a network call to POST /submissions/validate and returns the submission if successful', () => {
+      return validateSubmission(validSubmission, 'JSON', baseOptions)
+        .then((returnedSubmission) => {
+          sinon.assert.calledOnce(rpPostStub);
+          assert.strictEqual(rpPostStub.firstCall.args[0].url, '/submissions/validate');
+
+          assert.deepEqual(validSubmission, returnedSubmission);
+        });
+    });
+
+    it('throws an error if submissionFormat is something other than XML or JSON', () => {
+      return validateSubmission(validSubmission, 'FAKE', baseOptions)
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'Invalid format');
+        });
+    });
+
+    it('throws an error if the API returns anything other than a 201', () => {
+      rpPostStub.restore();
+      rpPostStub = sandbox.stub(rp, 'post').returns(new Promise((resolve, reject) => {
+        reject(new Error('Invalid Submission Object'));
+      }));
+
+      return validateSubmission(validSubmission, 'JSON', baseOptions)
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'Invalid Submission Object');
+        });
+    });
+
+    it('throws an error if there are no measurementSets to create', () => {
+      const validSubmissionNoMsets = Object.assign({}, validSubmission, {measurementSets: []});
+      rpPostStub.restore();
+      rpPostStub = sandbox.stub(rp, 'post').returns(new Promise((resolve, reject) => {
+        resolve(JSON.stringify({
+          data: {
+            submission: validSubmissionNoMsets
+          }
+        }));
+      }));
+
+      return validateSubmission(validSubmissionNoMsets, 'JSON', baseOptions)
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'At least one measurementSet must be defined to use this functionality');
+        });
+    });
+
+    it('will use the Submissions API to convert XML to JSON', () => {
+      // Don't need to actually send an XML submission here, just making
+      // sure that the "XML" submissionFormat will trigger the right
+      // header to be added
+      return validateSubmission(validSubmission, 'XML', baseOptions)
+        .then((returnedSubmission) => {
+          sinon.assert.calledOnce(rpPostStub);
+          assert.strictEqual(rpPostStub.firstCall.args[0].headers['Content-Type'], 'application/xml');
+          assert.strictEqual(rpPostStub.firstCall.args[0].headers['Accept'], 'application/json');
+        });
+    });
+  });
+
+  describe('getExistingSubmission', () => {
+    it('makes a network call to GET /submissions and returns a matching submission if there is one', () => {
+      const rpGetStub = sandbox.stub(rp, 'get').returns(new Promise((resolve, reject) => {
+        resolve(JSON.stringify({
+          data: {
+            submissions: [Object.assign({}, validSubmission, {id: '001'})]
+          }
+        }));
+      }));
+
+      return getExistingSubmission(validSubmission, baseOptions)
+        .then((returnedSubmission) => {
+          sinon.assert.calledOnce(rpGetStub);
+          assert.strictEqual(rpGetStub.firstCall.args[0].url, '/submissions?nationalProviderIdentifier=' + validSubmission.nationalProviderIdentifier);
+
+          assert.exists(returnedSubmission);
+          assert.strictEqual(returnedSubmission.id, '001');
+          assert.strictEqual(returnedSubmission.taxpayerIdentificationNumber, validSubmission.taxpayerIdentificationNumber);
+        });
+    });
+
+    it('throws an error if the API returns anything other than a 200', () => {
+      const rpGetStub = sandbox.stub(rp, 'get').returns(new Promise((resolve, reject) => {
+        reject(new Error('Could not fetch existing Submissions'));
+      }));
+
+      return getExistingSubmission(validSubmission, baseOptions)
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'Could not fetch existing Submissions');
+        });
+    });
+
+    it('throws an error if there are more than 1 matching submissions', () => {
+      const rpGetStub = sandbox.stub(rp, 'get').returns(new Promise((resolve, reject) => {
+        resolve(JSON.stringify({
+          data: {
+            submissions: [{
+              entityId: '123456',
+              entityType: 'individual'
+            }, {
+              entityId: '234567',
+              entityType: 'individual'
+            }]
+          }
+        }));
+      }));
+
+      return getExistingSubmission(validSubmission, baseOptions)
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'Could not determine which existing Submission matches request');
+        });
+    });
+  });
+
+  describe('putMeasurementSet', () => {
+    it('makes a network call to PUT /measurement-sets and returns a measurementSet if it was valid', () => {
+      const rpPutStub = sandbox.stub(rp, 'put').returns(new Promise((resolve, reject) => {
+        resolve(JSON.stringify({
+          data: {
+            measurementSet: validSubmission.measurementSets[0]
+          }
+        }));
+      }));
+
+      return putMeasurementSet(validSubmission.measurementSets[0], baseOptions, '001')
+        .then((mSet) => {
+          sinon.assert.calledOnce(rpPutStub);
+          assert.strictEqual(rpPutStub.firstCall.args[0].url, '/measurement-sets/001');
+
+          assert.exists(mSet);
+          assert.deepEqual(validSubmission.measurementSets[0], mSet);
+        });
+    });
+
+    it('throws an error if the API returns anything other than a 200', () => {
+      const rpPutStub = sandbox.stub(rp, 'put').returns(new Promise((resolve, reject) => {
+        reject(new Error('Random API Error'));
+      }));
+
+      return putMeasurementSet(validSubmission, baseOptions, '001')
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'Random API Error');
+        });
+    });
+
+  });
+
+  describe('postMeasurementSet', () => {
+    it('makes a network call to POST /measurement-sets and returns a measurementSet if it was valid', () => {
+      const rpPostStub = sandbox.stub(rp, 'post').returns(new Promise((resolve, reject) => {
+        resolve(JSON.stringify({
+          data: {
+            measurementSet: validSubmission.measurementSets[0]
+          }
+        }));
+      }));
+
+      return postMeasurementSet(validSubmission.measurementSets[0], baseOptions)
+        .then((mSet) => {
+          sinon.assert.calledOnce(rpPostStub);
+          assert.strictEqual(rpPostStub.firstCall.args[0].url, '/measurement-sets');
+
+          assert.exists(mSet);
+          assert.deepEqual(validSubmission.measurementSets[0], mSet);
+        });
+    });
+
+    it('throws an error if the API returns anything other than a 201', () => {
+      const rpPostStub = sandbox.stub(rp, 'post').returns(new Promise((resolve, reject) => {
+        reject(new Error('Random API Error'));
+      }));
+
+      return postMeasurementSet(validSubmission, baseOptions, '001')
+        .catch((err) => {
+          assert.throws(() => {throw err}, 'Random API Error');
+        });
+    });
+
+  });
+
+  describe('submitMeasurementSets', () => {
+    let rpPostStub;
+    let rpPutStub;
+    beforeEach(() => {
+      rpPostStub = sandbox.stub(rp, 'post').callsFake((mSet, options) => {
+        return new Promise((resolve, reject) => {
+          resolve(JSON.stringify({data: {measurementSet: mSet}}));
+        });
+      });
+      rpPutStub = sandbox.stub(rp, 'put').callsFake((mSet, options, mSetId) => {
+        return new Promise((resolve, reject) => {
+          resolve(JSON.stringify({data: {measurementSet: mSet}}));
+        });
+      });
+    });
+
+    it('will call POST for all new measurementSets and PUT for all existing measurementSets', () => {
+      const validSubmissionMoreMSets = Object.assign({}, validSubmission);
+      validSubmissionMoreMSets.measurementSets.push({
+        category: 'aci',
+        submissionMethod: 'claims',
+        performanceStart: '2017-01-01',
+        performanceEnd: '2017-06-01',
+        measurements: [{
+          measureId: 'ACI_HIE_3',
+          value: {
+            numerator: 2,
+            denominator: 3
+          }
+        }]
+      });
+
+      const existingSubmission = {
+        id: '001',
+        measurementSets: [{
+          category: 'ia',
+          submissionMethod: 'registry',
+          performanceStart: '2017-01-01',
+          performanceEnd: '2017-06-01',
+          measurements: [{
+            measureId: 'IA_EPA_4',
+            value: true
+          }]
+        }, {
+          category: 'aci',
+          submissionMethod: 'registry',
+          performanceStart: '2017-01-01',
+          performanceEnd: '2017-06-01',
+          measurements: [{
+            measureId: 'ACI_HIE_3',
+            value: {
+              numerator: 1,
+              denominator: 2
+            }
+          }]
+        }]
+      };
+
+      const promiseArray = submitMeasurementSets(existingSubmission, validSubmissionMoreMSets, {});
+      return Promise.all(promiseArray)
+        .then((promiseOutputs) => {
+          sinon.assert.calledOnce(rpPostStub)
+          sinon.assert.calledOnce(rpPutStub)
+        });
+    });
+  });
+});
