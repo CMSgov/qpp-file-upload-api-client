6790a39dc0a8305d0ac88d5513e373aea8fcc7fa
Josh <joshboley@gmail.com>
2017-10-13 17:01:43 -0400

    Removed lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export function fileUploader(submissionBody: any, submissionFormat: any, JWT: any, baseSubmissionURL: any, callback: any): any;
-
-export namespace fileUploader {
-    const prototype: {
-    };
-
-}
