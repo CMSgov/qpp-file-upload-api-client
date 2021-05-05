371f048c9b91441da664bcd45e9cd559cecda92f
Michael O'Keefe <michael.okeefe@semanticbits.com>
2021-05-05 13:13:35 -0400

    Fix typings typo

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-export function fileUploader: (submissionBody: any, submissionFormat: any, requestHeaders: any, baseSubmissionURL: any, callback: any) => any;
+export function fileUploader(submissionBody: any, submissionFormat: any, requestHeaders: any, baseSubmissionURL: any, callback: any): any;
